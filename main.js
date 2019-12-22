16023e9338b6d51cc3e36ccbc8f8b7df76b02611
Mc <350464517@qq.com>
2019-12-22 21:07:21 +0800

    [zmc]refactor: take some range-controling steps into function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var fixBox = document.getElementsByClassName("fixBox")[0];
 var movingBox = document.getElementsByClassName("movingBox")[0];
+var totalBox = document.getElementsByClassName("totalBox")[0];
 
 movingBox.onmousedown = function(event) {
     event = event || window.event;
@@ -13,27 +14,58 @@ movingBox.onmousedown = function(event) {
         movingBox.style.top = top - deltaTop + "px";
         var rangeLeft = left - deltaLeft;
         var rangeTop = top - deltaTop;
-        if (rangeLeft < 0) {
-            movingBox.style.left = "0px";
+        if (outOfRange(rangeLeft, rangeTop)) {
+            movingBox.style.left = rangeControl(rangeLeft, rangeTop)[0];
+            movingBox.style.top = rangeControl(rangeLeft, rangeTop)[1];
         }
-        if (rangeLeft > 920) {
-            movingBox.style.left = "920px";
-        }
-        if (rangeTop < 0) {
-            movingBox.style.top = "0px";
-        }
-        if (rangeTop > 720) {
-            movingBox.style.top = "720px";
-        }
-        if (rangeLeft > 220 && rangeLeft < 380 && rangeTop > 120 && rangeTop < 280) {
+        if (touchOthers(rangeLeft, rangeTop)) {
             fixBox.style.backgroundColor = "blue";
         }
-
-
     }
 
 }
 document.onmouseup = function() {
     document.onmousemove = null;
     document.onmousedown = null;
+}
+
+function outOfRange(leftPosition, topPosition) {
+    if (leftPosition < 0 || leftPosition > 920 || topPosition < 0 || topPosition > 720) {
+        return true;
+    }
+    return false;
+}
+
+function rangeControl(leftPosition, topPosition) {
+    var rangeReturn = [];
+    rangeReturn[0] = leftRangeControl(leftPosition);
+    rangeReturn[1] = topRangeControl(topPosition);
+    return rangeReturn;
+}
+
+function leftRangeControl(leftPosition) {
+    if (leftPosition < 0) {
+        return "0px";
+    } else if (leftPosition > 920) {
+        return "920px";
+    } else {
+        return leftPosition + "px"
+    }
+}
+
+function topRangeControl(topPosition) {
+    if (topPosition < 0) {
+        return "0px";
+    } else if (topPosition > 720) {
+        return "720px";
+    } else {
+        return topPosition + "px"
+    }
+}
+
+function touchOthers(leftPosition, topPosition) {
+    if (leftPosition > 220 && leftPosition < 380 && topPosition > 120 && topPosition < 280) {
+        return true;
+    }
+    return false;
 }
